0ccec318c156c38ff43b574138ae877b07150ed4
nurhat77 <nurhat7750@gmail.com>
2024-09-23 23:52:47 +0500

    feat: added new files

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        mainMethodShowMessage();
+        mainMethodShowInfo();
+    }
+
+    private static void mainMethodShowMessage() {
+        System.out.println("Hello able QA coures");
+    }
+
+    private static void mainMethodShowInfo() {
+        System.out.println("Lets learn Git!");
+    }
+}
